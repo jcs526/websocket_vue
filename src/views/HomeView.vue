8821f624863865445a96fc894b893fa866c34f84
Chansol Jeong <mutongzang@gmail.com>
2023-07-09 20:48:21 +0900

    웹소켓 클라이언트 사이드

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    {{ msg }}
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import type { Ref } from 'vue';
+import { io } from "socket.io-client";
+import axios from "axios";
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+const msg: Ref<string | number> = ref("");
+msg.value = "메세지입니다."
+
+interface State {
+  connected: boolean,
+  fooEvents: string[],
+  barEvents: string[]
+}
+
+const state: Ref<State> = ref({
+  connected: false,
+  fooEvents: [],
+  barEvents: []
 });
+
+const socket = io("http://localhost:3000");
+
+onMounted(async () => {
+  connect();
+  socket.on("test", (message) => {
+    console.log(message);
+    msg.value = message;
+  })
+});
+
+
+const connect = () => {
+  console.log("connet method");
+  socket.connect();
+  socket.emit("message", "message!!!!")
+}
+const disconnect = () => {
+  console.log("connet method");
+  socket.disconnect();
+}
+onBeforeUnmount(async () => {
+  disconnect();
+});
+
 </script>
+<style scoped></style>
